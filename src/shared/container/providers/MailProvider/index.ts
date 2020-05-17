600f4525c189dee7f83388aeb806b44d343d987c
Anderson Botelho Cerqueira <andersonbcdev@gmail.com>
2020-05-17 15:10:27 -0300

    finalizado upload de imagem para o S3

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -3,11 +3,11 @@ import { container } from 'tsyringe';
 import mailConfig from '@config/mail';
 import IMailProvider from './models/IMailProvider';
 
-import EtherealMailProvider from './implementations/EtherealMailProvider';
+// import EtherealMailProvider from './implementations/EtherealMailProvider';
 import SESMailProvider from './implementations/SESMailProvider';
 
 const providers = {
-  ethereal: container.resolve(EtherealMailProvider),
+  // ethereal: container.resolve(EtherealMailProvider),
   ses: container.resolve(SESMailProvider),
 };
 
